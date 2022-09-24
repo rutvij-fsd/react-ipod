213f560017ea5a0a96b5bf5b8099507e7cae561a
rutvij-fsd <rutvijpatel414@gmail.com>
2022-09-24 18:15:58 +0530

    Add  Zingtouch to capture rotation around wheel

--- src/Components/Statefull/App.js
+++ src/Components/Statefull/App.js
@@ -1,14 +1,30 @@
-
+import React from 'react';
 import '../Assets/CSS/App.css';
+import ZingTouch from 'zingtouch'
+
 import Ipod from '../Stateless/Ipod';
 
 
-function App() {
+class App extends React.Component {
+  // Function for rotateWheel event listeners
+  rotateWheel = () => {
+    var containerElement = document.getElementById('container');
+    var activeRegion = ZingTouch.Region(containerElement);
+    
+    activeRegion.bind(containerElement, "rotate", function (event) {
+        var newAngle = event.detail.angle;
+        console.log(newAngle);
+    })
+   
+  }
+
+  render(){
   return (
     <div className="App">
-     <Ipod/>
+     <Ipod rotateWheel={this.rotateWheel}/>
     </div>
   );
+  }
 }
 
 export default App;
--- src/Components/Stateless/Ipod.js
+++ src/Components/Stateless/Ipod.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import next from '../Assets/Images/next.png'
 import play_pause from '../Assets/Images/play-pause.png'
-const Ipod = () => {
+
+
+const Ipod = (props) => {
+
   return (
     // Main Outer Div
-    <div className='box-border h-72 w-36 bg-slate-400 mt-36 m-auto flex flex-col'>
+    <div className='box-border h-96 w-48 bg-slate-400 mt-20 m-auto flex flex-col'>
         {/* Display Div */}
-        <div className='box-border h-36 w-36 flex flex-col  bg-slate-200'></div>
+        <div className='box-border h-48 w-48 flex flex-col  bg-slate-200'></div>
         {/* Controller Div */}
-        <div className='box-border h-36 w-36 flex flex-col  bg-slate-300 justify-center items-center '>
-            <div className='rounded-full bg-white h-32 w-32 flex flex-col justify-center items-center relative'>
+        <div className='box-border h-48 w-48 flex flex-col  bg-slate-300 justify-center items-center'  >
+            {/* outer rounded div */}
+            <div className='rounded-full bg-white h-32 w-32 flex flex-col justify-center items-center relative' id='container' onMouseOver={props.rotateWheel} >
+                
                 <div className='rounded-full bg-slate-300 h-12 w-12 '>
                     <img className='h-6 absolute right-3 top-[50%] translate-y-[-50%] cursor-pointer' src={next} />
                     <img className='h-6 absolute left-3 top-[50%] translate-y-[-50%] rotate-180 cursor-pointer' src={next} />
